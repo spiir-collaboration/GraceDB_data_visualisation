f9ab6ab6bc489f52584859fc3bc6508d03b0ce41
Blair <44489920+EnSlavingBlair@users.noreply.github.com>
2019-04-06 10:13:38 +0800

    Replace with updated backup
    Most of the added functionality has been added back in that was nearly lost. 
    Things like, but not limited to:
    - the ability to track individual pipelines
    - writing the search parameters to file for use in another program
    - separating out file names for easier use in multiple ways
    - print statements to the user

--- get_events.py
+++ get_events.py
@@ -1,4 +1,4 @@
-# must execute source ~/.certrc firt
+# must execute source ~/.certrc first
 print("successfully opened document")
 import httplib
 from glue import gpstime
@@ -6,9 +6,9 @@ import sys
 import pdb
 
 try:
-	from ligo.gracedb.rest import GraceDb
+        from ligo.gracedb.rest import GraceDb
 except ImportError:
-	print >>sys.stderr, "Warning: gracedb import failed, program will crash if gracedb uploads are attempted"
+        print >>sys.stderr, "warning: gracedb import failed, program will crash if gracedb uploads are attempted"
 
 # set the gracedb server you want to download the events
 # playground is for testing, containing replay events back to Nov. 20, 2018
@@ -17,26 +17,34 @@ main_database = "gracedb" # either "gracedb" or "gracedb-playground"
 print("  Searching in database: "+main_database)
 gracedb_service_url = "https://"+main_database+".ligo.org/api/"
 gracedb_client = GraceDb(gracedb_service_url)
-# 
-# 
-# 
-# 
-# ???
-event_type = 'CBC 1186922482 .. 1187095282'
-event_name = event_type.replace(' ', '_')
+#print "response status %s" % r.json()
+#set the period/ condition of events, e.g. 1187008000 .. 1187009000 for the GW170817 event. Same format as input of the "Query" 
+# 1 day either side of known GW events:
+event_type = 'CBC 1126173062 .. 1126345862'#GW150914
+#event_type = 'gstlal pycbc spiir MBTAOnline 1128592500 .. 1128765300'#GW151012
+#event_type = 'CBC 1135049950 .. 1135222750'#GW151226
+#event_type = 'CBC 1167473536 .. 1167646336'#GW170104
+#event_type = 'CBC 1180836094 .. 1181008894'#GW170608
+#event_type = 'CBC 1185303407 .. 1185476207'#GW170729
+#event_type = 'gstlal MBTAOnline spiir 1186216119 .. 1186388919'#GW170809
+#event_type = 'gstlal MBTAOnline spiir 1186655461 .. 1186828261'#GW170814
+#event_type = 'gstlal MBTAOnline spiir 1186922482 .. 1187095282'#GW170817
+#event_type = 'gstlal MBTAOnline spiir 1186971927 .. 1187144727'#GW170818
+#event_type = 'CBC 1187442856 .. 1187615656'#GW170823
+event_name = event_type.replace(' ', '_') # to get rid of those pesky white spaces
 print('  for events satisfying: '+event_type)
-events = gracedb_client.events(event_type) # number of events consistent with piir Nov 20 - Nov 31
+events = gracedb_client.events(event_type) # number of events consistent with spiir Nov 20 - Nov 31
 with open('graceid-pipeline_'+event_name,'w') as f:
-	for event in events:
-		gid = event['graceid']
-		pipe = event['pipeline']
-		f.write(gid+' '+pipe+'\n')
-		fname = "%s.xml" % gid
-		fout = open(fname, 'w+')
-		content = gracedb_client.files(gid, filename = 'coinc.xml')
-		fout.write(content.read())
-		fout.close()
+        for event in events:
+                gid = event['graceid']
+                pipe = event['pipeline']
+                f.write(gid+' '+pipe+'\n')
+                fname = "%s.xml" % gid
+                fout = open(fname, 'w+')
+                content = gracedb_client.files(gid, filename="coinc.xml")
+                fout.write(content.read())
+                fout.close()
 
 with open("name_log", "w") as f:
-	f.write(main_database+"\n")
-	f.write(event_name)
+        f.write(main_database+"\n")
+        f.write(event_name)
